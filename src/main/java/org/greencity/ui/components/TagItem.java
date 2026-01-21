602e3e571fd3b001f80795ed6d8f58a23f520dce
olha-hoshko <olha.hoshko.w@gmail.com>
2026-01-21 14:22:48 +0200

    replaced select and remove with click method. made isSelected public.

--- src/main/java/org/greencity/ui/components/TagItem.java
+++ src/main/java/org/greencity/ui/components/TagItem.java
@@ -19,25 +19,13 @@ public class TagItem extends BaseComponent {
         return name.getText();
     }
 
-    protected boolean isSelected() {
+    public boolean isSelected() {
         String classes = closeIcon.getAttribute("class");
 
         return classes != null && classes.contains("global-tag-close-icon");
     }
 
-    public void select() {
-        if (!isSelected()) {
-            rootElement.click();
-        } else {
-            throw new IllegalStateException("Tag is already selected.");
-        }
-    }
-
-    public void remove() {
-        if (isSelected()) {
-            rootElement.click();
-        } else {
-            throw new IllegalStateException("Can't remove a tag that's not selected.");
-        }
+    public void click() {
+        rootElement.click();
     }
 }
