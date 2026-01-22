fa2e959b3202de618b9cac13e52ba1b1e94520d0
YAROSLAV <yaroslav101serykh@gmail.com>
2026-01-22 12:50:08 +0200

    Added open, isVisible, click methods and FooterComponent to BasePage

---
+++ src/main/java/org/greencity/ui/components/AuthModal/FooterComponent.java
@@ -0,0 +1,16 @@
+package org.greencity.ui.components.AuthModal;
+
+import org.greencity.ui.components.BaseComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FooterComponent extends BaseComponent {
+
+    public FooterComponent(WebDriver driver) {
+        super(driver);
+    }
+
+    public FooterComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
