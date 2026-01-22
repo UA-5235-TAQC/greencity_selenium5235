ccbfe6bbc9d664e0a7dafccf6d9a2ba51f923982
EvanShabarovskiy <ivanshabarovskij@gmail.com>
2026-01-22 11:45:19 +0200

    Implementing HeaderComponent is done

---
+++ src/main/java/org/greencity/ui/components/AuthModal/SignInModal.java
@@ -0,0 +1,10 @@
+package org.greencity.ui.components.AuthModal;
+
+import org.openqa.selenium.WebDriver;
+
+public class SignInModal extends AuthModalBase {
+
+    public SignInModal(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/main/java/org/greencity/ui/pages/EcoNewsPage.java
@@ -0,0 +1,11 @@
+package org.greencity.ui.pages;
+
+import org.greencity.ui.pages.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class EcoNewsPage extends BasePage {
+
+    public EcoNewsPage(WebDriver driver) {
+        super(driver);
+    }
+}
