938cf1cceb8124330eb8e179c7764c01aee63851
YAROSLAV <yaroslav101serykh@gmail.com>
2026-01-22 13:39:43 +0200

    Added HomePage class representing the main page of GreenCity website.

--- src/main/java/org/greencity/ui/pages/HomePage.java
+++ src/main/java/org/greencity/ui/pages/HomePage.java
@@ -1,9 +1,96 @@
 package org.greencity.ui.pages;
 
+import org.greencity.ui.components.StatRowComponent;
+import org.greencity.ui.components.SubscribeComponent;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HomePage extends BasePage {
+
+    protected List<StatRowComponent> statRowComponentList;
+    private SubscribeComponent subscribeComponent;
+
+    @FindBy(css = "main")
+    protected WebElement root;
+
+    @FindBy(css = ".main-content h1")
+    protected WebElement heroTitle;
+
+    @FindBy(css = "#header-left p")
+    protected WebElement heroDescription;
+
+    @FindBy(css = "#header-left button.primary-global-button")
+    protected WebElement startHabitButton;
+
+    @FindBy(css = "#stats")
+    protected WebElement statsSection;
+
+    @FindBy(css = "#events")
+    protected WebElement ecoNewsSection;
+
+    @FindBy(css = ".subscribe-container")
+    protected WebElement subscriptionSection;
+
+    @FindBy(css = ".subscription-input")
+    protected WebElement emailInput;
+
+    @FindBy(css = "div #subscribe")
+    protected WebElement subscribeButton;
+
+    @FindBy(css = ".eco-events a") // Посилання "Read all news"
+    protected WebElement readAllNewsLink;
+
     public HomePage(WebDriver driver) {
         super(driver);
+    }
+
+    @Override
+    public boolean isPageOpened() {
+        return isVisible(root);
+    }
+
+    public void open() {
+        driver.get(BASE_URL);
+    }
+
+    public String getHeroTitle() {
+        return getText(heroTitle);
+    }
+
+    public String getHeroDescription() {
+        return getText(heroDescription);
+    }
+
+    public void clickStartHabit() {
+        click(startHabitButton);
+    }
+
+    public String getStats() {
+        return getText(statsSection);
+    }
+
+    public String getEcoNewsPreview() {
+        return getText(ecoNewsSection);
+    }
+
+    public void clickReadAllNews() {
+        click(readAllNewsLink);
+    }
+
+    public void subscribe(String email) {
+        waitUntilVisible(emailInput);
+        emailInput.sendKeys(email);
+        click(subscribeButton);
+    }
+
+    public List<StatRowComponent> getStatRowComponentList() {
+        return statRowComponentList;
+    }
+
+    public SubscribeComponent getSubscribeComponent() {
+        return subscribeComponent;
     }
 }
