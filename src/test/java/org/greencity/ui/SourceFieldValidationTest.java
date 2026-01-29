af104e92a29e29f849c79923210517347d67acc8
Liubomyr halamaha <GALAMAGAL86@gmail.com>
2026-01-29 15:54:59 +0200

    update

--- src/test/java/org/greencity/ui/SourceFieldValidationTest.java
+++ src/test/java/org/greencity/ui/SourceFieldValidationTest.java
@@ -18,45 +18,45 @@ import java.util.List;
 public class SourceFieldValidationTest extends BaseTestRunner {
 
     private CreateNewsPage createNewsPage;
-    private EcoNewsPage ecoNewsPage;
 
     @BeforeClass
     public void LoginUser() {
         HomePage homePage = new HomePage(driver);
         loginUser(homePage);
-        createNewsPage = new CreateNewsPage(driver);
-        ecoNewsPage = new EcoNewsPage(driver);
+        createNewsPage = homePage.getHeader()
+                .clickEcoNewsLink()
+                .clickCreateNews();
     }
 
-    @BeforeMethod
-    public void beforeMethod() {
-        waitUntilAuthModalDisappear();
-    }
 
     @Test
     public void SourceFieldValidation() {
         // Navigate and create news item with valid data and empty Source Field
-        createNewsPage
-                .getHeader()
+        createNewsPage.getHeader()
                 .clickEcoNewsLink()
                 .clickCreateNews()
-                .enterTitle("A".repeat(170))
-                .enterContent("A".repeat(1000))
-                .selectTags(List.of(
-                        EcoNewsTag.NEWS.getTagName(),
-                        EcoNewsTag.EVENTS.getTagName()));
+                .createNews(
+                        "A".repeat(170),
+                        List.of(
+                                EcoNewsTag.NEWS.getEn(),
+                                EcoNewsTag.EVENTS.getEn()),
+                        "",
+                        "A".repeat(1000),
+                        null
+                );
+
 
         // Verify that publish button is "Enabled" and data is valid
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(createNewsPage.getTitleCounterText(), "170/170", "Title length doesn't match");
         softAssert.assertEquals(createNewsPage.getTitleValue(), "A".repeat(170), "Title text doesn't match");
-        softAssert.assertEquals(createNewsPage.getContent(), "A".repeat(1000), "The content field text doesn't match");
+        softAssert.assertEquals(createNewsPage.getContentComponent().getContent(), "A".repeat(1000), "The content field text doesn't match");
         softAssert.assertTrue(createNewsPage.isPublishButtonEnabled(), "Publish Button is not enabled");
         softAssert.assertAll();
 
         // Publish
         createNewsPage.clickPublish();
-
+        EcoNewsPage ecoNewsPage = new EcoNewsPage(driver);
         // Verify that success message is appears and matches the given text
         ecoNewsPage.waitForMessageAppear();
         Assert.assertEquals(ecoNewsPage.getMessageText(), "Your news has been successfully published");
@@ -64,12 +64,15 @@ public class SourceFieldValidationTest extends BaseTestRunner {
 
         // Create new news item with non-valid Source Field data
         ecoNewsPage.clickCreateNews()
-                .enterTitle("W".repeat(20))
-                .enterContent("W".repeat(100))
-                .enterSource("W".repeat(20))
-                .selectTags(List.of(
-                        EcoNewsTag.EDUCATION.getTagName(),
-                        EcoNewsTag.ADS.getTagName()));
+                .createNews(
+                        "W".repeat(20),
+                        List.of(
+                                EcoNewsTag.EDUCATION.getEn(),
+                                EcoNewsTag.ADS.getEn()),
+                        "W".repeat(20),
+                        "W".repeat(100),
+                        null
+                );
 
        // Verify that publish button is "Disabled"
        Assert.assertFalse(createNewsPage.isPublishButtonEnabled(), "Button is active, but should be disabled");
@@ -83,9 +86,5 @@ public class SourceFieldValidationTest extends BaseTestRunner {
        // Verify that success message is appears and matches the given text
        ecoNewsPage.waitForMessageAppear();
        Assert.assertEquals(ecoNewsPage.getMessageText(), "Your news has been successfully published");
-    }
-
-    public void waitUntilAuthModalDisappear() {
-        createNewsPage.wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("app-auth-modal"))));
     }
 }
