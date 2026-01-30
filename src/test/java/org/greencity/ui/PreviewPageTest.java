1dff81d92491c65d053c96ee07bc15bac97ab0a6
EvanShabarovskiy <ivanshabarovskij@gmail.com>
2026-01-30 11:47:19 +0200

    Test have been updated

--- src/test/java/org/greencity/ui/PreviewPageTest.java
+++ src/test/java/org/greencity/ui/PreviewPageTest.java
@@ -5,10 +5,15 @@ import org.greencity.ui.pages.HomePage;
 import org.greencity.ui.pages.MySpace.MySpaceHabitsTabPage;
 import org.greencity.ui.pages.NewsPreviewPage;
 import org.greencity.ui.testrunners.BaseTestRunner;
+import org.greencity.utils.TestValueProvider;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class PreviewPageTest extends BaseTestRunner {
     CreateNewsPage createNewsPage;
@@ -41,7 +46,18 @@ public class PreviewPageTest extends BaseTestRunner {
                 .enterContent(newsText)
                 .clickPreview();
 
+        Assert.assertEquals(driver.getCurrentUrl(), testValueProvider.getBaseUIGreenCityUrl() + "/news/preview");
         Assert.assertEquals(previewPage.getNewsTitle(), newsTitle);
         Assert.assertEquals(previewPage.getNewsText(), newsText);
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d, yyyy").withLocale(Locale.US);;
+        String expectedDate = today.format(formatter);
+        Assert.assertEquals(previewPage.getNewsCreatingDate(), expectedDate);
+        Assert.assertEquals(previewPage.getAuthorName(), testValueProvider.getUserName());
+
+        previewPage.clickBackToCreateNewsBtn();
+        Assert.assertEquals(driver.getCurrentUrl(), testValueProvider.getBaseUIGreenCityUrl() + "/news/create-news");
+        //There will be an error because of page components loading bug
+        Assert.assertTrue(createNewsPage.isPageOpened());
     }
 }
