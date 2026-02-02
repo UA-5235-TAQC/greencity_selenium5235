28f72498bc941d8fad9fce702a65dd158af7667c
olha-hoshko <olha.hoshko.w@gmail.com>
2026-02-02 17:45:17 +0200

    added step annotation to TagItem

--- src/main/java/org/greencity/ui/components/TagItem.java
+++ src/main/java/org/greencity/ui/components/TagItem.java
@@ -1,5 +1,6 @@
 package org.greencity.ui.components;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,16 +17,18 @@ public class TagItem extends BaseComponent {
         super(driver, rootElement);
     }
 
+    @Step("Get tag name")
     public String getName() {
         return name.getText();
     }
 
+    @Step("Verify if tag is selected")
     public boolean isSelected() {
         String classes = closeIcon.getAttribute("class");
         return classes != null && classes.contains("global-tag-close-icon");
     }
 
-    // NOTE: tried to use rootElement.click() here, but somehow it doesn't work.
+    @Step("Click on tag")
     public void click() {
         name.click();
     }
